d0dbceda717f1465c37f6f07066f267bd92f603b
Bruno Oliveira <brun0.0nline@pm.me>
2025-08-25 01:04:08 -0300

    First full setup of OS

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,23 +2,90 @@
 
 set -ouex pipefail
 
-### Install packages
+########## DNF ##########
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+echo 'fastestmirror=1' | tee -a /etc/dnf/dnf.conf
+echo 'max_parallel_downloads=10' | tee -a /etc/dnf/dnf.conf
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf config-manager setopt fedora-cisco-openh264.enabled=1
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y in distribution-gpg-keys && \
+rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-free-fedora-$(rpm -E %fedora) && \
+rpmkeys --import /usr/share/distribution-gpg-keys/rpmfusion/RPM-GPG-KEY-rpmfusion-nonfree-fedora-$(rpm -E %fedora) && \
+dnf5 -y --setopt=localpkg_gpgcheck=1 in https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+                                        https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-#### Example for enabling a System Unit File
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 
-systemctl enable podman.socket
+dnf5 copr enable scottames/ghostty
+
+########## PACKAGES ##########
+
+PKGS=(
+    # Hardware support
+    @hardware-support
+
+    # Graphical Interface
+    gdm
+    gnome-session-wayland-session
+    adw-gtk3
+    @fonts
+
+    # Audio support
+    pipewire
+    wireplumber
+
+    # Multimedia, hardware acceleration and codecs
+    ffmpeg
+    libva-intel-driver
+    @multimedia
+
+    # Needed for homebrew, git and some more
+    @development-tools 
+    
+    # CLI tools
+    distrobox 
+    micro
+
+    # Graphical tools
+    nautilus
+    ghostty
+    chromium
+
+    # Flatpaks
+    flatpak
+    gnome-software
+)
+
+dnf5 -y in --setopt="install_weak_deps=False" "${PKGS[@]}"
+
+########## Flatpak ##########
+
+flatpak remote-delete --all
+flatpak remote-add flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+########## GNOME ##########
+
+gsettings set org.gnome.desktop.peripherals.mouse middle-click-emulation true
+gsettings set org.gnome.desktop.interface font-hinting 'full'
+
+########## Systemd ##########
+
+SYSTEMD=(
+    gdm.service
+    flatpak-maintenance.timer
+    podman.socket
+)
+
+for UNIT in "${SYSTEMD[@]}"; do
+    systemctl enable "$UNIT"
+done
+
+systemctl set-default graphical.target
+
+########## Cleanup Repos ##########
+
+dnf5 copr disable scottames/ghostty
+
+find /etc/yum.repos.d/ -maxdepth 1 -type f -name '*.repo' ! -name 'fedora.repo' ! -name 'fedora-updates.repo' ! -name 'fedora-updates-testing.repo' -exec rm -f {} +
